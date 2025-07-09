f8c8d1e828290fc42392498cad62507097dde6f1
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-07-09 14:00:41 +0900

    Update Event.java

--- src/main/berry/api/eventbus/Event.java
+++ src/main/berry/api/eventbus/Event.java
@@ -4,17 +4,12 @@ public abstract class Event {
     protected boolean cancelled = false;
     public boolean cancellable () {
         // Cancellable by default, except for post events.
-        if (this instanceof DoubleEvent event) return event.isPreEvent ();
-        else return true;
-    }
-    public void cancel () {
-        setCancelled (true);
-    }
-    public void setCancelled (boolean c) {
-        if (!this.cancellable () && c) throw new IllegalStateException ("Event cannot be cancelled!");
-        else this.cancelled = c;
+        return this instanceof CancellableEvent;
     }
     public boolean cancelled () {
-        return cancelled;
+        if(this instanceof CancellableEvent)
+            return this.cancelled;
+        else
+            return false;
     }
 }
