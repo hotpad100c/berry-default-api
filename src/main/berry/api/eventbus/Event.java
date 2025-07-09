5c3d85d5340507077a00de59ace6cecd32dc3295
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-07-09 14:11:34 +0900

    Update Event.java

--- src/main/berry/api/eventbus/Event.java
+++ src/main/berry/api/eventbus/Event.java
@@ -1,12 +1,16 @@
 package berry.api.eventbus;
 
+import berry.api.eventbus.CancellableEvent;
 public abstract class Event {
     protected boolean cancelled = false;
     public boolean cancellable () {
         //Only cancellable when marked.
         return this instanceof CancellableEvent;
     }
+    public void cancel() {
+        if (this instanceof CancellableEvent cancellableEvent) cancellableEvent.setCancelled(true);
+    }
     public boolean cancelled () {
-      return this instanceof CancellableEvent cancellableEvent ? ((cancellableEvent)this).isCancelled():false;
+      return (this instanceof CancellableEvent cancellableEvent) ? cancellableEvent.isCancelled():false;
     }
 }
